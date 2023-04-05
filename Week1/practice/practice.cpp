9466cedd18776a952ff3137d19af9a4ccd313dc4
Subrat Mishra <piyush.subrat8@gmail.com>
2023-04-05 09:56:09 +0530

    a

---
+++ Week1/practice/practice.cpp
@@ -0,0 +1,188 @@
+#include<iostream>
+using namespace std;
+
+
+// int main(){
+//     cout<<"Hello World";
+// }
+
+
+// int main(){
+//     int x=5;
+//     cout<<x<<endl;
+//     x=x+5;
+//     cout<<x;
+// }
+
+// int main(){
+//     int x=6;
+//     int y=2;
+//     int z=x/y;
+//     cout<<z<<endl;
+// }
+
+
+// int main(){
+//     int x=5;
+//     cout<<x<<endl;
+//     cout<<++x<<endl;
+//     cout<<x<<endl;
+//     cout<<x++<<endl;
+//     cout<<x<<endl;
+// }
+
+
+
+// int main(){
+//     int a = 13; //a mean dividend
+//     int b = 3; // b is divisor
+//     // int q = a/b; // q is quotient
+//     // int r; // r is reminder
+
+//     // r = a - (b*q);
+
+//     int r = a%b;
+
+//     cout<<r;
+// }
+
+
+// int main(){
+//     float x1=40;
+//     float x2=45;
+//     float x3=20;
+//     float x4=49;
+//     float x5=50;
+
+//     float z = (x1+x2+x3+x4+x5)/5;
+//     cout<<z;
+// }
+
+
+// int main(){
+//     float p=3.14;
+//     float r=6;
+
+//     float z=p*r*r;
+//     cout<<z;
+// }
+
+
+
+// int main(){
+//     //si=p*r*t/100
+
+//     float p=1000;
+//     float r=2.2;
+//     float t=2;
+
+//     float si=(p*r*t)/100;
+//     cout<<si;
+// }
+
+
+
+
+// int main(){
+//     int x;
+//     cout<<"enter your number:";
+//     cin>>x;
+//     cout<<"your number is: "<<x;
+
+// }
+
+
+
+
+// int main(){
+//     int x;
+//     cout<<"enter your nuber: ";
+//     cin>>x;
+//     int y=x*x;
+//     cout<<"Sqaure of the number is :"<<y;
+// }
+
+
+// int main(){
+//     float r=3.14;
+//     int x;
+//     cout<<"Enter your number:";
+//     cin>>x;
+//     float ans=r*x*x;
+//     cout<<"your answer is:"<<ans;
+
+// }
+
+
+
+// int main(){
+//     int x,y;
+//     cout<<"Enter your first number";
+//     cin>>x;
+//     cout<<"Enter your second number:";
+//     cin>>y;
+//     int ans=x+y;
+//     cout<<"Ans is:"<<ans;
+
+// }
+
+
+
+
+// int main(){
+//     int p,q;
+//     cout<<"Enter the value of p,q:";
+//     cin>>p>>q;
+//     cout<<"p="<<p<<"q="<<q;
+
+// }
+
+
+
+// int main(){
+//     float x,y,z;
+//     cout<<"p:";
+//     cin>>x;
+//     cout<<"i:";
+//     cin>>y;
+//     cout<<"t:";
+//     cin>>z;
+
+//     float si=(x*y*z)/100;
+//     cout<<si;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x=3;
+//     cout<<(x=7)<<endl;
+//     cout<<(x==7);
+// }
+
+
+// int main(){
+//     char ch;
+//     cin>>ch;
+//     int x=int(ch)-64;
+//     cout<<"number of char:"<<x;
+// }
+
+
+// int main(){
+//     char ch;
+//     cin>>ch;
+//     int x=int(ch)-64;
+//     cout<<"number of char:"<<x;
+// }
+
+int main(){
+    bool x=2;
+    int y=(int)x+4;
+    cout<<y<<endl;
+    char ch='a';
+    int a=(int)ch;
+    cout<<a;
+
+}
